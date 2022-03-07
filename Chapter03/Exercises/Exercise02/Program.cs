3be6d935a36ca847c3153c3f17260253555f2b60
Domenico Zaffora <zaffora@gmail.com>
2022-03-07 15:44:17 -0500

    Chapter 3
    finished with chapter 3

---
+++ Chapter03/Exercises/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+checked
+{
+  try
+  {
+    int max = 500;
+    for (byte i = 0; i < max; i++)
+    {
+      WriteLine(i);
+    }
+  }
+  catch (OverflowException ex)
+  {
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+  }
+}
