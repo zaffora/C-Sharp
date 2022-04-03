512eca1df1b6a439181cae16d6bbe4ee9cfa2d6a
Domenico Zaffora <zaffora@gmail.com>
2022-04-03 19:03:16 -0400

    working on IDE
    working on IDE

---
+++ Chapter03/CoPilotTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+
+var doc = XDocument.Load("index.xhml");
+
+// find all images 
+var images = doc.Descendants("img");
+// and give them a red boarder 
+foreach (var image in images)
+{
+    image.SetAttributeValue("border", "1");
+    image.SetAttributeValue("style", "border-color:red");
+}
---
+++ Chapter03/CoPilotTest/CoPilotTest/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int CalculateDaysBetweenDates(DateTime startDate, DateTime endDate)
+{
+    var totalDays = 0;
+    for (var currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
+    {
+        if (currentDate.DayOfWeek != DayOfWeek.Saturday && currentDate.DayOfWeek != DayOfWeek.Sunday)
+        {
+            totalDays++;
+        }
+    }
+    return totalDays;
+}
